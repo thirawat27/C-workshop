324306a5d1bc62a8a4f5eac0312862a6a1feb5ac
thirawat27 <thunthirawas@gmail.com>
2022-11-21 20:32:24 +0700

    first commit

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //library 
+
+int main() {
+  int Long;
+  int Width;
+  printf("Input of Long :"); //input data
+  scanf("%d", &Long);
+  printf("Input of Width : "); 
+  scanf("%d", &Width );
+  printf("Square_ area is : %d\n",Long*Width ); // output result
+  return 0;
+}
